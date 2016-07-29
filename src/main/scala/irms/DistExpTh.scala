5eeb3306ced86b89797f9d05c3d4841146f6a237
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-07-30 00:53:04 +0800

    set up spark

--- src/main/scala/irms/DistExpTh.scala
+++ src/main/scala/irms/DistExpTh.scala
@@ -1,7 +1,12 @@
+import org.apache.spark.sql._
+import org.apache.spark._
+
 package irms {
     object DistExpTh {
         def main(args: Array[String]):Unit = {
-            println("Hello World")
+            val session = SparkSession.builder.appName("03_create_mid_struct_table").getOrCreate()
+            import session.implicits._
+            val path = "/home/gaoxiang/create-dataset-for-ir/outputs/"
         }
     }
 }
