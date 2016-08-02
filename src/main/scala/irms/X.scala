2c1682436c7ac00e68420830b908831337d19907
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-08-02 09:12:56 -0400

    change to symlink

--- src/main/scala/irms/X.scala
+++ src/main/scala/irms/X.scala
@@ -1,9 +1,1 @@
-package irms {
-    // info of x
-    // TODO: import values of xmin,xmax,xstep from 03_create_mid_struct_table.scala
-    object X {
-        val xmin = 670 //included
-        val xmax = 3702 //included
-        val xstep = 4
-    }
-}
+../../../../../create-dataset-for-ir/X.scala
