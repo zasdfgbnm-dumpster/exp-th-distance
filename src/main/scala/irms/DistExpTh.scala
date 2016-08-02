0b5d203d9db7098b78533cf3f111f79309628c98
Xiang Gao <qasdfgtyuiop@gmail.com>
2016-08-02 08:02:05 -0400

    fixes

--- src/main/scala/irms/DistExpTh.scala
+++ src/main/scala/irms/DistExpTh.scala
@@ -8,7 +8,7 @@ package irms {
         def main(args: Array[String]):Unit = {
             val session = SparkSession.builder.appName("03_create_mid_struct_table").getOrCreate()
             import session.implicits._
-            val path = "/home/gaoxiang/create-dataset-for-ir/outputs/"
+            val path = "/home/gaoxiang/irms/create-dataset-for-ir/outputs/"
         }
     }
 }
--- src/main/scala/irms/LineShape.scala
+++ src/main/scala/irms/LineShape.scala
@@ -1,18 +1,21 @@
 package irms {
 
-    abstract class LineShape(expir:Array[Float], thir:Array[Float]) {
+    abstract class LineShape(expir:Array[Float], thirpeaks:Array[(Float,Float)]) {
 
         // TODO: What is the relationship between IR intensity in km/mol and TRANSMITTANCE?
         //       How do we convert these values?
 
         // information of single peak
         val nparams1:Int
-        def f1(freq:Float,max:Foat)(gparams:Array[Float],params1:Array[Float])(x:Float):Float
-        def derivative1(freq:Float,max:Foat)(gparams:Array[Float],params1:Array[Float])(x:Float):(Array[Float],Array[Float])
+        def f1(freq:Float,max:Float)(gparams:Array[Float],params1:Array[Float])(x:Float):Float
+        def derivative1(freq:Float,max:Float)(gparams:Array[Float],params1:Array[Float])(x:Float):(Array[Float],Array[Float])
 
         // info of x
         // TODO: import values of xmin,xmax,xstep from 03_create_mid_struct_table.scala
-        val xs = Range(xmin,xmax+xstep,xstep)
+        val xmin = 670 //included
+        val xmax = 3702 //included
+        val xstep = 4
+        val xs = Range(xmin,xmax+xstep,xstep).toArray
 
         // Note: these functions are implemented assuming that the IR intensity and the
         //       TRANSMITTANCE have the same unit
@@ -20,12 +23,12 @@ package irms {
 
         // theoretical spectrum vector
         // formula:
-        // thir(xi) = sum( j from 0 to m, f1(freqj,maxj)(param1j)(xi) )
-        def thir(gparams:Array[Float], params:Array[Float]):Array[Float] = {
+        // thirvec(xi) = sum( j from 0 to m, f1(freqj,maxj)(param1j)(xi) )
+        def thirvec(gparams:Array[Float], params:Array[Float]):Array[Float] = {
             // split params into [param1]
-            val param1s = params.sliding(nparams1,nparams1)
+            val param1s = params.sliding(nparams1,nparams1).toArray
             // apply (freq,max) to f1
-            val fs = thir.map(j => f1(j._1,j._2))
+            val fs = thirpeaks.map(j => f1(j._1,j._2) _)
             // apply (gparams,params1) to f1(freq,max)
             val fparams = fs.zip(param1s).map(j=>j._1(gparams,j._2))
             // calculate sum( j from 0 to m, f1(freq,max)(gparams,params1j)(xi) )
@@ -36,7 +39,7 @@ package irms {
         // formula:
         // euclidean_loss = sum( i from 0 to n, ( thir(xi)-expir(xi) ) ^ 2 )
         def euclidean_loss(gparams:Array[Float], params:Array[Float]):Float = {
-            thir(gparams,params).zip(expir).map(j=>(j._1-j._2)*(j._1-j._2)).reduce(_+_)
+            thirvec(gparams,params).zip(expir).map(j=>(j._1-j._2)*(j._1-j._2)).reduce(_+_)
         }
 
         // d(euclidean_loss)/d(parameters)
@@ -47,13 +50,13 @@ package irms {
         //                    = sum{ i from 0 to n, j from 0 to m, 2 * ( thir(xi)-expir(xi) ) * derivative1(freqj,maxj)(gparams,param1j)(xi)._1 }
         def derivative_euclidean_loss(gparams:Array[Float], params:Array[Float]):(Array[Float],Array[Float]) = {
             // split params into [param1]
-            val param1s = params.sliding(nparams1,nparams1)
+            val param1s = params.sliding(nparams1,nparams1).toArray
             // apply (freq,max) to derivative1
-            val derivatives = thir.map(j => derivative1(j._1,j._2))
+            val derivatives = thirpeaks.map(j => derivative1(j._1,j._2) _)
             // apply (gparams,params1) to derivative1(freq,max)
-            val dparams = fs.zip(param1s).map(j=>j._1(gparams,j._2))
+            val dparams = derivatives.zip(param1s).map(j=>j._1(gparams,j._2))
             // calculate 2 * ( thir(xi)-expir(xi) ) for all xi
-            val diff2 = thir(gparams,params).zip(expir).map(j=>2*(j._1-j._2))
+            val diff2 = thirvec(gparams,params).zip(expir).map(j=>2*(j._1-j._2))
             // calculate elementwise plus/multiplies of two array:
             def cplus[C](a1:C,a2:C):C = ((a1,a2).asInstanceOf[(Any,Any)] match {
                 case (a1:Array[Float],a2:Array[Float]) => a1.zip(a2).map(j=>j._1+j._2)
@@ -64,32 +67,32 @@ package irms {
                 case c:(Any,Any) => (cmult[Any](c._1,scalar),cmult[Any](c._2,scalar))
             }).asInstanceOf[C]
             // calculate the sum{ i from 0 to n,.... }
-            def sumi_calculator(d:Float=>Array[Float]):Array[Float] = {
-                val diff2deriv = diff2.zip(xs).map(cmult(j=>d(j._2)._2,j._1))
-                diff2deriv.reduce(cplus)
+            def sumi_calculator(d:Float=>(Array[Float],Array[Float])):(Array[Float],Array[Float]) = {
+                val diff2deriv = diff2.zip(xs).map((j:(Float,Int))=>cmult(d(j._2),j._1))
+                diff2deriv.reduce(cplus[(Array[Float],Array[Float])])
             }
-            val sumi = dparams.map(sumi)
-            ( sumi.map(_._2).reduce(cplus), sumi.flatMap(_._1) )
+            val sumi = dparams.map(sumi_calculator)
+            ( sumi.map(_._2).reduce(cplus[Array[Float]]), sumi.flatMap(_._1) )
         }
 
     }
 
-    private class ForcedOscillatior(expir:Array[Float], thir:Array[Float]) extends LineShape {
-        override val nparams1:Int = 1
-
-        // formula A = F / sqrt( (f0^2-f^2)^2 + 4*beta^2*f^2 )
-        //         E = A^2
-        def f1(freq:Float,max:Foat)(gparams:Array[Float], params1:Array[Float])(x:Float):Float = {
-            val beta = params(0)
-            val b2f24 = 4 * beta*beta * freq*freq
-            val FF = max * b2f24
-            val diff2freq = freq*freq - x*x
-            FF / ( diff2freq*diff2freq + b2f24 )
-        }
-
-        // formula dE/d(beta) = ...
-        def derivative1(freq:Float,max:Foat)(gparams:Array[Float], params1:Array[Float])(x:Float):Array[Float] = {
-            //TODO: implement
-        }
-    }
+    // private class ForcedOscillatior(expir:Array[Float], thir:Array[(Float,Float)]) extends LineShape {
+    //     override val nparams1:Int = 1
+    //
+    //     // formula A = F / sqrt( (f0^2-f^2)^2 + 4*beta^2*f^2 )
+    //     //         E = A^2
+    //     def f1(freq:Float,max:Foat)(gparams:Array[Float], params1:Array[Float])(x:Float):Float = {
+    //         val beta = params1(0)
+    //         val b2f24 = 4 * beta*beta * freq*freq
+    //         val FF = max * b2f24
+    //         val diff2freq = freq*freq - x*x
+    //         FF / ( diff2freq*diff2freq + b2f24 )
+    //     }
+    //
+    //     // formula dE/d(beta) = ...
+    //     def derivative1(freq:Float,max:Float)(gparams:Array[Float], params1:Array[Float])(x:Float):Array[Float] = {
+    //         //TODO: implement
+    //     }
+    // }
 }
